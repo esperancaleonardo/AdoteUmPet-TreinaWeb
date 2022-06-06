5f94b6041173b76875ebfee78764b4456cdc7c9c
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-06-06 11:33:21 -0300

    adoption app configurado

---
+++ api/adoption/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from .models import Adoption
+from pet.serializers import PetSerializer
+from pet.models import Pet
+
+
+class AdoptionSerializer(serializers.ModelSerializer):
+    pet = PetSerializer(many = False, read_only = True)
+    pet_id = serializers.PrimaryKeyRelatedField(
+        many = False,
+        write_only = True,
+        queryset = Pet.objects.all()
+    )
+
+    class Meta:
+        model = Adoption
+        fields = ('id', 'value', 'email', 'pet', 'pet_id')
+
+    def create(self, validated_data):
+        validated_data['pet'] = validated_data.pop('pet_id')
+        return super().create(validated_data)
+
+    @staticmethod
+    def validate_value(value):
+        if value < 10:
+            raise serializers.ValidationError('O valor de doação deve ser maior que R$10,00')
+
+        if value > 150:
+            raise serializers.ValidationError('O valor de doação deve ser menor que R$150,00')
+
+        return value
---
+++ api/adoption/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+from .serializers import AdoptionSerializer
+
+# Create your views here.
+
+
+class AdoptionList(APIView):
+
+    def post(self, request, format=None):
+        serializer = AdoptionSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = HTTP_400_BAD_REQUEST)
