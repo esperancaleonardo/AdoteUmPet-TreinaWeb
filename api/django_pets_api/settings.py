a99697521e599ab1665d356600e4075b61288a1a
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-06-06 16:38:07 -0300

    Erro CORS front-back

--- api/django_pets_api/settings.py
+++ api/django_pets_api/settings.py
@@ -125,4 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
--- pets/src/pages/index.tsx
+++ pets/src/pages/index.tsx
@@ -7,43 +7,28 @@ import {
   Button,
   Snackbar
 } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Title from '../ui/components/Title/Title'
 import List from '../ui/components/List/List'
 import { Pet } from '../data/@types/pet'
+
+import { ApiService } from '../data/services/ApiService'
 
 const Home: NextPage = () => {
   const [modalOpen, setModalOpen] = useState<Pet | null>(null)
   const [modalSent, setModalSent] = useState(false)
   const [email, setEmail] = useState('')
   const [value, setValue] = useState('')
-  const [petsList, setPetsList] = useState([
-    {
-      id: 1,
-      name: 'Bidu',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas impedit sed magnam mollitia dolor excepturi perspiciatis dolores doloremque provident qui. Ut unde, dolores facere culpa dolorum obcaecati labore delectus deleniti.',
-      image:
-        'https://www.mundoecologia.com.br/wp-content/uploads/2019/11/Bidu-da-Turma-da-M%C3%B4nica.jpg'
-    },
-    {
-      id: 2,
-      name: 'Coragem',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas impedit sed magnam mollitia dolor excepturi perspiciatis dolores doloremque provident qui. Ut unde, dolores facere culpa dolorum obcaecati labore delectus deleniti.',
-      image:
-        'https://rollingstone.uol.com.br/media/stories/episodios-assustadores-de-coragem-o-cao-covarde/assets/5.jpeg'
-    },
-    {
-      id: 3,
-      name: 'Mutley',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas impedit sed magnam mollitia dolor excepturi perspiciatis dolores doloremque provident qui. Ut unde, dolores facere culpa dolorum obcaecati labore delectus deleniti.',
-      image:
-        'https://images.tcdn.com.br/img/img_prod/697730/adesivo_lateral_vidro_caminhao_carro_decorativo_mutley_1147486506_1_10004d4a25c5e024890ecfacd8c09c3b.jpg'
-    }
-  ])
+  const [petsList, setPetsList] = useState<Pet[]>([])
+
+  useEffect(() => {
+    console.log('useEffect() executou')
+    ApiService.get('/pets').then(pets => {
+      console.log(pets.data)
+      setPetsList(pets.data)
+    })
+  }, [])
 
   function adopt() {
     setModalOpen(null)
