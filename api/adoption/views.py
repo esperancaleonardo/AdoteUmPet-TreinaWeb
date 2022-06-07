7da66bea14efd98f52a0c426c1e3ee81dfe76df4
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-06-06 23:59:19 -0300

    rota de listagem das adocoes

--- api/adoption/views.py
+++ api/adoption/views.py
@@ -1,23 +1,29 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
+
+from .models import Adoption
 from .serializers import AdoptionSerializer
+from .email_service import send_confirmation_mail
 
 # Create your views here.
 
 
 class AdoptionList(APIView):
-
     def post(self, request, format=None):
-        serializer = AdoptionSerializer(data = request.data)
+        serializer = AdoptionSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = HTTP_201_CREATED)
+            adoption = serializer.save()
+            send_confirmation_mail(adoption)
+            return Response(serializer.data, status=HTTP_201_CREATED)
 
         return Response(
-            {
-                'errors': serializer.errors,
-                'message': 'Erro de validação dos dados.'
-            },
-            status = HTTP_400_BAD_REQUEST)
+            {"errors": serializer.errors, "message": "Erro de validação dos dados."},
+            status=HTTP_400_BAD_REQUEST,
+        )
+
+    def get(self, request, format=None):
+        adoptions = Adoption.objects.all()
+        serializer = AdoptionSerializer(adoptions, many = True)
+        return Response(serializer.data, status=HTTP_200_OK)
