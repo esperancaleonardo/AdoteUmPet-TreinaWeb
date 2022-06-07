805ea0ec82d96d92dd783d908843fea41601096f
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-06-06 23:59:38 -0300

    rota de criação/cadastro de pet no app

--- api/pet/views.py
+++ api/pet/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
+
 from .models import Pet
 from .serializers import PetSerializer
 
@@ -8,8 +9,22 @@ from .serializers import PetSerializer
 
 
 class PetList(APIView):
-
     def get(self, request, format=None):
         pets = Pet.objects.all()
         serializer = PetSerializer(pets, many=True)
-        return Response(serializer.data, status = HTTP_200_OK)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = PetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=HTTP_200_OK)
+
+        return Response(
+            {
+                "message": "Erro na validação da criação do Pet",
+                "errors": serializer.errors,
+            },
+            status=HTTP_400_BAD_REQUEST,
+        )
