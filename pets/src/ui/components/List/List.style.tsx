347f0968ffd6d36af8b8cf544146fcdb1d8ab84d
Leonardo <leonardo.oliveira.esperanca@gmail.com>
2022-06-10 00:10:12 -0300

    ajustes nos estilos

--- pets/src/ui/components/List/List.style.tsx
+++ pets/src/ui/components/List/List.style.tsx
@@ -1,8 +1,8 @@
 import { styled } from '@mui/material'
 
 export const ListStyled = styled('ul')`
-  width: 100%;
-  max-width: 800px;
+  width: 85%;
+  max-width: 680px;
   margin: 0 auto;
   padding: ${({ theme }) => theme.spacing(3)};
 `
@@ -10,10 +10,16 @@ export const ListStyled = styled('ul')`
 export const ListItemStyled = styled('li')`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  border-radius: 14px;
+  background-color: #ffccbb;
+  box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: ${({ theme }) => theme.spacing(3)};
   gap: ${({ theme }) => theme.spacing(5)};
   margin-bottom: ${({ theme }) => theme.spacing(5)};
 
   ${({ theme }) => theme.breakpoints.down('md')} {
+    max-width: 490px;
+    min-width: 370px;
     grid-template-columns: 1fr;
     gap: ${({ theme }) => theme.spacing(2)};
     margin-bottom: ${({ theme }) => theme.spacing(10)};
@@ -23,6 +29,15 @@ export const ListItemStyled = styled('li')`
 export const ItemPicStyled = styled('img')`
   width: 350px;
   height: 350px;
+  border-radius: 50px;
+  box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.04);
+  }
+
+  //border: 1pt solid black;
 
   ${({ theme }) => theme.breakpoints.down('md')} {
     width: 100%;
--- pets/src/ui/components/Title/Title.style.tsx
+++ pets/src/ui/components/Title/Title.style.tsx
@@ -1,14 +1,18 @@
 import { styled } from '@mui/material'
 
 export const TitleStyled = styled('h1')`
-  font-size: 20px;
+  font-size: 24px;
   text-align: center;
+  max-width: 80%;
+  margin-inline: auto;
+  word-break: break-world;
   margin-top: ${({ theme }) => theme.spacing(5)};
 `
 
 export const SubitleStyled = styled('h2')`
   font-size: 18px;
   text-align: center;
+  margin-inline: ${({ theme }) => theme.spacing(9)};
   margin-bottom: ${({ theme }) => theme.spacing(5)};
   font-weight: normal;
   color: ${({ theme }) => theme.palette.text.secondary};
